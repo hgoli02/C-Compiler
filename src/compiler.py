346b37cfa63a2dbd705b47fea421c6460d900600
hgoli02 <79108091+hgoli02@users.noreply.github.com>
2023-03-20 16:12:19 +0330

    final update?

--- src/compiler.py
+++ src/compiler.py
@@ -12,11 +12,14 @@ if __name__ == '__main__':
         if output[0] == 'Invalid input':
             if (scanner.reader.file_ended):
                 break
-            output_string = "(" + str(output[0]) + ', ' + str(output[1]) + ")"
-            output_dict[scanner.reader.get_lineno()].append(output_string)
+            output_string = "(" + str(output[1]) + ', ' + str(output[0]) + ")"
+            lexical_dict[scanner.reader.get_lineno()].append(output_string)
         elif output[0] == Type.ERROR1 or output[0] == Type.ERROR2 or output[0] == Type.ERROR3:
-            output_string = "(" + str(output[0].value) + ', ' + str(output[1]) + ")"
-            output_dict[scanner.reader.get_lineno()].append(output_string)
+            tmp = str(output[1])
+            if len(tmp) >= 7:
+                tmp = tmp[:7] + '...'
+            output_string = "(" + tmp + ', ' + str(output[0].value) + ")"
+            lexical_dict[scanner.reader.get_lineno()].append(output_string)
         else:
             output_string = "(" + str(output[0].value) + ', ' + str(output[1]) + ")"
             output_dict[scanner.reader.get_lineno()].append(output_string) 
@@ -32,16 +35,21 @@ if __name__ == '__main__':
             token_print += ' ' #TODO: check last space
         token_print += '\n'
     
-    print(token_print)
-
+    #print(token_print)
+    f = open('tokens.txt', 'w')
+    f.write(token_print)
+    f.close()
 
     #Symbol table
     symbol_table_print = ''
     for i in range(len(scanner.symbol_table)):
         symbol_table_print += str(i + 1) + '.\t' + scanner.symbol_table[i] + '\n'
     
-    print(symbol_table_print)
-
+    #print(symbol_table_print)
+    f = open('symbol_table.txt', 'w')
+    f.write(symbol_table_print)
+    f.close()
+    
     lexical_print = ''
     for key in lexical_dict.keys():
         lexical_print += f'{key}.\t'
@@ -51,4 +59,7 @@ if __name__ == '__main__':
 
     if lexical_print == '':
         lexical_print = 'There is no lexical error.'
-    print(lexical_print)
+    #print(lexical_print)
+    f = open('lexical_errors.txt', 'w')
+    f.write(lexical_print)
+    f.close()
